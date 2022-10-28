9ba567ce265cd7245e0960cc8027e5ddf02d3bbb
AanirudhMehra <aanirudhmehra@gmail.com>
2022-10-28 06:25:36 +0530

    completed 6 challenges

--- app.js
+++ app.js
@@ -16,11 +16,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/', function(req,res){
-  res.render("home",{firstContent: homeStartingContent})
+  res.render("home",{firstContent:homeStartingContent});
+
 });
 
+app.get('/about' , function(req,res){
+  res.render("about",{aboutContent:aboutContent});
+});
 
-
+app.get('/contact' , function(req,res){
+  res.render("contact",{contactmeContent:contactContent});
+});
 
 
 
